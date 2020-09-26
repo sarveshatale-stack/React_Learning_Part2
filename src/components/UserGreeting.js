1cc5c836990f9e10a3cb288b18d3502ff0954db8
sarveshatale-stack <71748819+sarveshatale-stack@users.noreply.github.com>
2020-09-26 13:36:26 +0530

    React rendering

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -8,19 +8,37 @@ class UserGreeting extends Component {
     };
   }
   render() {
-    if (this.state.IsloggedIn) {
-      return (
-        <div>
-          <h1>Welcome User</h1>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <h1>Welcome Guest</h1>
-        </div>
-      );
-    }
+    //Variable Approach
+    // let condition;
+
+    // if (this.state.IsloggedIn) {
+    //   condition = <div>Welcome ABC User</div>;
+    // } else {
+    //   condition = <div>Please login</div>;
+    // }
+
+    //If-Else Approach
+    // if (this.state.IsloggedIn) {
+    //   return (
+    //     <div>
+    //       <h1>Welcome User</h1>
+    //     </div>
+    //   );
+    // } else {
+    //   return (
+    //     <div>
+    //       <h1>Welcome Guest</h1>
+    //     </div>
+    //   );
+    // }
+    //Terninal Approach
+    // return this.state.IsloggedIn ? (
+    //   <div>Welcome ABC User</div>
+    // ) : (
+    //   <div>Please Log In</div>
+    // );
+    //SHort Circuit Approach
+    return this.state.IsloggedIn && <div>Welcome ABC User</div>;
   }
 }
 export default UserGreeting;
